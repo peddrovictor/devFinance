c516cfbd68cb5f9c69a07103ec298988ed087608
Pedro <pedrodbt8@gmail.com>
2021-02-25 21:28:19 -0300

    Edição de JS

--- script.js
+++ script.js
@@ -22,20 +22,21 @@ const transactions = [
         id: 1,
         description: 'Luz',
         amount: -50000,
-        date: '23/01/2021'
+        date: '23/01/2021',
     },
     {
         id: 2,
         description: 'Website',
         amount: 50000,
-        date: '23/01/2021'
+        date: '23/01/2021',
     },
     {
         id: 3,
         description: 'Internet',
         amount: -20000,
-        date: '23/01/2021'
-    }]
+        date: '23/01/2021',
+    },
+]
 
 const Transaction = {
     incomes() {
@@ -54,19 +55,31 @@ const Transaction = {
 
 
 const DOM = {
-    innerHTMLTransaction() {
 
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    
+    innerHTMLTransaction(transaction) {
         const html = `
-                    <tr>
-                        <td class="description">Luz</td>
-                        <td class="expense">-R$ 500,00</td>
-                        <td class="date">23/01/2021</td>
-                        <td>
-                            <img src="./assets/minus.svg" alt="Remover transação">
-                        </td>
-                    </tr>
+                <td class="description">${transaction.description}</td>
+                <td class="expense">${transaction.amount}</td>
+                <td class="date">${transaction.date}</td>
+                <td>
+                    <img src="./assets/minus.svg" alt="Remover transação">
+                </td>
                     `
-        
+        return html
 
     }
 }
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})

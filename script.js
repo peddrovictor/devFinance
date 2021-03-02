a395352c8eb99c79d3b65e165b40a623c80b1ee2
Pedro <pedrodbt8@gmail.com>
2021-03-01 22:22:26 -0300

    Implementação do sistema de cálculo

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ const transactions = [
 
 const Transaction = {
     incomes() {
-        //somar entradas
+        return "aqui"
     },
     expenses() {
-        //soma saídas
+        return "cheguei"
     },
     total() {
-        //entradas - saídas
+        return "discovery"
     }
 }
 
@@ -69,9 +69,11 @@ const DOM = {
     innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
                 <td class="description">${transaction.description}</td>
-                <td class="${CSSclass}">${transaction.amount}</td>
+                <td class="${CSSclass}">${amount}</td>
                 <td class="date">${transaction.date}</td>
                 <td>
                     <img src="./assets/minus.svg" alt="Remover transação">
@@ -79,9 +81,43 @@ const DOM = {
                     `
         return html
 
+        
+
+    },
+
+    updateBalance(){
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Transaction.incomes()
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Transaction.expenses()
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Transaction.total()
+    }
+
+}
+
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : ""
+        
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value)/100
+        
+        value = value.toLocaleString("pt-br",{
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
     }
 }
 
 transactions.forEach(function(transaction){
     DOM.addTransaction(transaction)
 })
+
+DOM.updateBalance()
